02f6f7ae98a2bad0a045f4cabb0c909d99ddd14c
KIMHYEONGJIN5925 <93527060+KIMHYEONGJIN5925@users.noreply.github.com>
2022-05-28 11:41:27 +0900

    3. Spring Template

--- joyce-kotlin-practice/app/src/main/java/com/example/kotlinpractice/sample1.kt
+++ joyce-kotlin-practice/app/src/main/java/com/example/kotlinpractice/sample1.kt
@@ -6,9 +6,21 @@ fun main() {
     println(add(4,5)) // 강의에서는 a:4, b:5 로 콜론인데 왜 여기선 안되지
     // 아 콜론을 사용한게아니라 그냥 파라미터에 (4,5) 요로케 넣어주면 자동으로 저게 생김
     // println(add(a=4,b=5)) 이방식도 계산은 되는데..
+
+    //3. String Template
+    val name = "hyeongjin"
+    val lastname = "Kim"
+    println("my name is ${name} and I'm 29 ") // 변수사용
+    println("my name is ${name + lastname} and I'm in korea")
+
+    // println("this is 2$a")
+    println("this is 2\$a")
+    // 문자앞에 백슬래시 넣으면 무조건 문자로 인식함 $ 기호 사용의 예
+
+    //4. 조건식식
 }
 
-// 1. 함수
+// . 함수
 
 fun helloWorld () { //return type 이 없는 경우에는 : Unit을 생략해도 된다. = redundant다.
     println("Hello World!")
@@ -16,6 +28,26 @@ fun helloWorld () { //return type 이 없는 경우에는 : Unit을 생략해도
 
 //java나 c언어와 다른점은 변수가 뒤에온다는점 = 변수 이름 변수타입 순
 fun add (a : Int, b : Int) : Int { // 파라미터 a의 타입설정, 파라미터 b의 타입 설정, 리턴타입의 설정, 변수타입 첫번째 대문자!
+    // return type 이 있으니까 콜론 즉, Unit을 생략하면 안됨
     return a+b
 }
+
+// 2. val vs var
+// val = value : 값, 바뀌지않음, 재할당 불가능
+// var = variable : 변수, 바뀜, 재할당 가능
+
+fun hi() {
+    val a : Int = 10
+    var b : Int = 9
+
+    b = 100
+    // 바로 할당할 때에는 변수타입을 자동추론하지만, 바로 할당하지 않을 때에는 변수타입을 선언해야함.
+    val c = 100
+    var d = 100
+
+    var name : String = "hyeongjin"
+}
+
+
+
 
